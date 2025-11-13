2ef4c19cc21eee43c0c78e6e96d42362837b832b
Anurag-Mondal <anurag2862@gmail.com>
2025-11-13 22:35:29 +0530

    .

--- x.c
+++ x.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
-int main () {
-    int m;
-    printf ("Hello");
+
+int factorial (int x) {
+    if (x == 0) return 1;
+    return x * factorial(x - 1);
+}
+
+void a (int* ans, int n) {
+    *ans = factorial(n);
+    return;
+}
+
+int main() {
+    int n, ans;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    a (&ans, n);
+
+    printf("Factorial = %d\n", ans);
+
     return 0;
 }
