09470ed096ed869060e47dde45bbfd980d10b048
Anurag-Mondal <anurag2862@gmail.com>
2025-12-18 15:06:16 +0530

    C Notes

---
+++ Recursion/Reverse_A_Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void reverse (int n) {
+    if (n == 0) return;
+    printf ("%d",n % 10);
+    n /= 10;
+    reverse (n);
+}
+int main () {
+    int n;
+    printf ("Enter a number: ");
+    scanf ("%d",&n);
+    printf ("The reverse is: ");
+    reverse (n);
+    return 0;
+}
